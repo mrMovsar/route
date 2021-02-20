448b35aa60b38abe91ed583906859050d7021c21
Movsar <movsar1991.mb@gmail.com>
2021-02-20 18:30:28 +0300

    add yandex map

--- src/ContactMoscow.js
+++ src/ContactMoscow.js
@@ -1,12 +1,14 @@
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 const ContactMoscow = () => (
+  
   <YMaps >
-    <div >
       Moscow, Cremlin! 
-      <Map defaultState={{ center: [43.324718, 45.692447], zoom: 14 }} />
-    </div>
+      <Map state={{ center: [43.324727, 45.692417], zoom: 14 }} width="100%" height="500px" >
+      <Placemark geometry={[43.32472782067527,45.69241746197129]} />
+      </Map>
   </YMaps>
+ 
 );
   
   export default ContactMoscow;
